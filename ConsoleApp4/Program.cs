90ee5fe497eb5c3e186a7d30d672b28cb3857d44
ablik07 <karolina.koptu@bk.ru>
2025-10-19 22:43:34 +0300

    предметы

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -27,4 +27,61 @@ namespace TextRPG
         public abstract void Use(Player player);
     }
 
-  
+    // Класс оружия
+    public class Weapon : Item
+    {
+        public int Attack { get; private set; }
+
+        public Weapon(string name, int value, int attack) : base(name, value)
+        {
+            Attack = attack;
+        }
+
+        public override void Use(Player player)
+        {
+            player.EquipWeapon(this);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (Атака: {Attack})";
+        }
+    }
+
+    // Класс доспехов
+    public class Armor : Item
+    {
+        public int Defense { get; private set; }
+
+        public Armor(string name, int value, int defense) : base(name, value)
+        {
+            Defense = defense;
+        }
+
+        public override void Use(Player player)
+        {
+            player.EquipArmor(this);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (Защита: {Defense})";
+        }
+    }
+
+    // Класс зелья здоровья
+    public class HealthPotion : Item
+    {
+        public HealthPotion() : base("Зелье здоровья", 15) { }
+
+        public override void Use(Player player)
+        {
+            player.Heal();
+            Console.WriteLine("Вы выпили зелье здоровья и полностью восстановили HP!");
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (Полное восстановление здоровья)";
+        }
+    }
