65886a65f57d5259dfd0127d8decbd60c0cc7342
ablik07 <karolina.koptu@bk.ru>
2025-10-19 22:46:55 +0300

    главный класс игры

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -277,4 +277,228 @@ namespace TextRPG
     }
 
     // Главный класс игры
- 
+    public class Game
+    {
+        private Player player;
+        private Random random;
+        private int turnCount;
+
+        // Списки предметов
+        private List<Weapon> weapons = new List<Weapon>
+        {
+            new Weapon("меч просто", 10, 5),
+            new Weapon("круче меч", 20, 8),
+            new Weapon("лук", 25, 10),
+            new Weapon("Магический посох", 30, 12),
+            new Weapon("суперпупер мэч", 50, 15)
+        };
+
+        private List<Armor> armors = new List<Armor>
+        {
+            new Armor("броня", 10, 3),
+            new Armor("Кольчуга", 20, 5),
+            new Armor("оспехи", 30, 8),
+            new Armor("штаны крутые", 25, 6),
+            new Armor("мегапупер броня", 50, 12)
+        };
+
+        public Game()
+        {
+            player = new Player(100);
+            random = new Random();
+            turnCount = 0;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Добро пожаловать в текстовую RPG игру!");
+            Console.WriteLine("Каждый ход вы будете встречать либо сундук, либо врага.");
+            Console.WriteLine("Каждые 10 ходов вас ждет встреча с боссом!\n");
+
+            while (player.HP > 0)
+            {
+                turnCount++;
+                Console.WriteLine($"\n=== Ход {turnCount} ===");
+                Console.WriteLine(player.GetStatus());
+
+                if (player.IsFrozen)
+                {
+                    Console.WriteLine("Вы заморожены и пропускаете ход!");
+                    player.IsFrozen = false;
+                    ContinueGame();
+                    continue;
+                }
+
+                // Случайное событие: 50% сундук, 50% враг
+                if (random.Next(2) == 0)
+                {
+                    OpenChest();
+                }
+                else
+                {
+                    FightEnemy();
+                }
+
+                // Каждые 10 ходов - босс
+                if (turnCount % 10 == 0)
+                {
+                    Console.WriteLine("\n⚠️ Приближается босс! ⚠️");
+                    FightBoss();
+                }
+
+                if (player.HP > 0)
+                {
+                    ContinueGame();
+                }
+            }
+
+            Console.WriteLine("\n=== Игра окончена ===");
+            Console.WriteLine($"Вы продержались {turnCount} ходов!");
+        }
+
+        private void OpenChest()
+        {
+            Console.WriteLine("Вы нашли сундук!");
+
+            // Случайный предмет
+            Item item = random.Next(3) switch
+            {
+                0 => new HealthPotion(),
+                1 => weapons[random.Next(weapons.Count)],
+                2 => armors[random.Next(armors.Count)],
+                _ => new HealthPotion()
+            };
+
+            Console.WriteLine($"В сундуке: {item}");
+
+            if (item is HealthPotion)
+            {
+                item.Use(player);
+            }
+            else
+            {
+                Console.WriteLine("\nВаша текущая экипировка:");
+                Console.WriteLine(player.GetEquipment());
+                Console.WriteLine("\nХотите взять этот предмет? (д/н)");
+
+                if (Console.ReadLine().ToLower() == "д")
+                {
+                    item.Use(player);
+                    Console.WriteLine("Предмет экипирован!");
+                }
+                else
+                {
+                    Console.WriteLine("Вы выбросили предмет.");
+                }
+            }
+        }
+
+        private void FightEnemy()
+        {
+            var enemy = EnemyFactory.CreateRegularEnemy();
+            Console.WriteLine($"Вы встретили {enemy.Name}!");
+            StartCombat(enemy);
+        }
+
+        private void FightBoss()
+        {
+            var boss = EnemyFactory.CreateBoss();
+            Console.WriteLine($"Перед вами {boss.Name}!");
+            StartCombat(boss);
+        }
+
+        private void StartCombat(Enemy enemy)
+        {
+            Console.WriteLine($"\n=== Бой с {enemy.Name} ===");
+
+            while (enemy.IsAlive && player.HP > 0)
+            {
+                // Ход игрока
+                PlayerTurn(enemy);
+                if (!enemy.IsAlive) break;
+
+                // Ход врага
+                EnemyTurn(enemy);
+            }
+
+            if (!enemy.IsAlive)
+            {
+                Console.WriteLine($"\nПобеда! Вы победили {enemy.Name}!");
+            }
+        }
+
+        private void PlayerTurn(Enemy enemy)
+        {
+            Console.WriteLine("\nВаш ход:");
+            Console.WriteLine("1 - Атаковать");
+            Console.WriteLine("2 - Защищаться");
+            Console.Write("Выберите действие: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    int damage = player.Attack;
+                    enemy.TakeDamage(damage);
+                    Console.WriteLine($"Вы нанесли {damage} урона {enemy.Name}!");
+                    Console.WriteLine($"{enemy.GetStatus()}");
+                    break;
+                case "2":
+                    Console.WriteLine("Вы готовитесь к защите...");
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор, вы пропускаете ход!");
+                    break;
+            }
+        }
+
+        private void EnemyTurn(Enemy enemy)
+        {
+            Console.WriteLine($"\nХод {enemy.Name}:");
+
+            int damage = enemy.CalculateDamage(player);
+            int finalDamage = damage;
+
+            // Проверка защиты игрока
+            if (player.TryDodge())
+            {
+                Console.WriteLine("Вы полностью уклонились от атаки!");
+            }
+            else if (!enemy.IgnoreDefense)
+            {
+                finalDamage = player.CalculateBlockedDamage(damage);
+                Console.WriteLine($"{enemy.Name} наносит {finalDamage} урона! (Исходный урон: {damage})");
+            }
+            else
+            {
+                Console.WriteLine($"{enemy.Name} игнорирует вашу защиту и наносит {finalDamage} урона!");
+            }
+
+            player.TakeDamage(finalDamage);
+
+            // Проверка заморозки
+            if (enemy.TryFreezePlayer())
+            {
+                player.IsFrozen = true;
+                Console.WriteLine($"{enemy.Name} замораживает вас! Вы пропустите следующий ход.");
+            }
+
+            Console.WriteLine($"{player.GetStatus()}");
+        }
+
+        private void ContinueGame()
+        {
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+    }
+
+    // Главная программа
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
